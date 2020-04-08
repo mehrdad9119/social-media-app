ad5ab6e33a3f8d209dcfc0aa7340bbcfdb413db5
mehrdad9119 <m248163264128>
2020-04-07 18:45:51 -0700

    finished backend

--- functions/utils/FBAuth.js
+++ functions/utils/FBAuth.js
@@ -1,9 +1,9 @@
-const admin = require('./admin')
+const { admin, db } = require('./admin')
 
 module.exports = (req, res, next) => {
     let idToken;
     if (req.headers.authorization && req.headers.authorization.startsWith('Bearer ')) {
-        idToken = req.headers.authorization.split('Beare ')[1];
+        idToken = req.headers.authorization.split('Bearer ')[1];
     } else {
         console.error('No token found')
         return res.status(403).json({ error: 'Unauthorized' })
@@ -19,6 +19,7 @@ module.exports = (req, res, next) => {
         })
         .then(data => {
             req.user.handle = data.docs[0].data().handle
+            req.user.imageUrl = data.docs[0].data().imageUrl
             return next()
         })
         .catch((err) => {
